f4842b6f345e1999bb8cfca59e0d88458c7ec9d1
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 04:57:32 +0500

    Update Game.cpp

---
+++ src/Game.cpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+#include "Player.hpp"
+#include "Enemy.hpp"
+#include "Bullet.hpp"
+#include "Particle.hpp"
+
+class Game {
+public:
+    Game();
+    void run();
+    
+private:
+    void processEvents();
+    void update(float dt);
+    void render();
+    void spawnEnemy();
+    void checkCollisions();
+    
+    sf::RenderWindow window;
+    sf::Clock clock;
+    sf::Font font;
+    sf::Text scoreText;
+    
+    std::unique_ptr<Player> player;
+    std::vector<std::unique_ptr<Bullet>> bullets;
+    std::vector<std::unique_ptr<Enemy>> enemies;
+    std::vector<std::unique_ptr<Particle>> particles;
+    
+    float spawnTimer = 0;
+    float spawnInterval = 1.5f;
+    int score = 0;
+    bool gameOver = false;
+};
