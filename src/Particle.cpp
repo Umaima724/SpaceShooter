bf2de5e0630dd96ca7ba77c26399d2c5bed314f7
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 05:01:59 +0500

    Update Particle.cpp

---
+++ src/Particle.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Entity.hpp"
+#include "Particle.hpp"
+
+class Particle : public Entity {
+public:
+    Particle(sf::Vector2f pos, sf::Color color, float lifetime);
+    void update(float dt) override;
+    void draw(sf::RenderWindow& window) const override;
+    sf::FloatRect getBounds() const override { return {}; }
+    
+private:
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+    float lifetime;
+    float maxLifetime;
+};
+
+
+Particle::Particle(sf::Vector2f pos, sf::Color color, float life) 
+    : lifetime(life), maxLifetime(life) {
+    shape.setRadius(3 + rand() % 4);
+    shape.setFillColor(color);
+    shape.setPosition(pos);
+    
+    float angle = (rand() % 360) * 3.14159f / 180.f;
+    float speed = 50 + rand() % 150;
+    velocity = {std::cos(angle) * speed, std::sin(angle) * speed};
+}
+
+void Particle::update(float dt) {
+    shape.move(velocity * dt);
+    lifetime -= dt;
+    
+    // Fade out
+    sf::Color c = shape.getFillColor();
+    c.a = static_cast<sf::Uint8>(255 * (lifetime / maxLifetime));
+    shape.setFillColor(c);
+    
+    if (lifetime <= 0) alive = false;
+}
+
+void Particle::draw(sf::RenderWindow& window) const {
+    window.draw(shape);
+}
