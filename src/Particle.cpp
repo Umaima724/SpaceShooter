213d63968a8daec92a00592317b6ed74df716113
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 05:12:42 +0500

    Update Particle.cpp

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,13 +1,14 @@
 #include "Particle.hpp"
+#include <cmath>
 
 Particle::Particle(sf::Vector2f pos, sf::Color color, float life) 
     : lifetime(life), maxLifetime(life) {
-    shape.setRadius(3 + rand() % 4);
+    shape.setRadius(2 + rand() % 5);
     shape.setFillColor(color);
     shape.setPosition(pos);
     
     float angle = (rand() % 360) * 3.14159f / 180.f;
-    float speed = 50 + rand() % 150;
+    float speed = 80 + rand() % 200;
     velocity = {std::cos(angle) * speed, std::sin(angle) * speed};
 }
 
@@ -15,9 +16,8 @@ void Particle::update(float dt) {
     shape.move(velocity * dt);
     lifetime -= dt;
     
-    // Fade out
     sf::Color c = shape.getFillColor();
-    c.a = static_cast<sf::Uint8>(255 * (lifetime / maxLifetime));
+    c.a = static_cast<<sf::Uint8>(255 * (lifetime / maxLifetime));
     shape.setFillColor(c);
     
     if (lifetime <= 0) alive = false;
