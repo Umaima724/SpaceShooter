471b91c3e46d08bbc90194fec5de9ad299c4919b
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 05:14:17 +0500

    Update Utils.hpp

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,14 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-// Helper to get center of FloatRect
-inline sf::Vector2f operator|(const sf::FloatRect& rect, int) {
+inline sf::Vector2f getCenter(const sf::FloatRect& rect) {
     return {rect.left + rect.width / 2.f, rect.top + rect.height / 2.f};
-}
-
-// Extension to FloatRect for center access
-namespace sf {
-    inline Vector2f FloatRect::getCenter() const {
-        return {left + width / 2.f, top + height / 2.f};
-    }
 }
