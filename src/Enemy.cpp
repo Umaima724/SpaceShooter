16c6c145946875f39bffbff793f37275e01cf006
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 05:00:47 +0500

    Update Enemy.cpp

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "Entity.hpp"
+#include "Enemy.hpp"
+#include <cmath>
 
 class Enemy : public Entity {
 public:
@@ -16,3 +18,27 @@ private:
     float time = 0;
     float startX;
 };
+
+Enemy::Enemy(sf::Vector2f pos) {
+    shape.setRadius(20);
+    shape.setFillColor(sf::Color::Red);
+    shape.setOrigin(20, 20);
+    shape.setPosition(pos);
+    startX = pos.x;
+}
+
+void Enemy::update(float dt) {
+    time += dt;
+    float sway = std::sin(time * swayFrequency) * swayAmplitude;
+    shape.setPosition(startX + sway, shape.getPosition().y + speed * dt);
+    
+    if (shape.getPosition().y > 620) alive = false;
+}
+
+void Enemy::draw(sf::RenderWindow& window) const {
+    window.draw(shape);
+}
+
+sf::FloatRect Enemy::getBounds() const {
+    return shape.getGlobalBounds();
+}
