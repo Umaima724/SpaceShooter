e3600fb280781fdc43baf291c8ef18ce51cd73cb
Umaima724 <umaimamubashar19@gmail.com>
2026-05-28 05:06:45 +0500

    Update Enemy.cpp

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,23 +1,5 @@
-#pragma once
-#include "Entity.hpp"
 #include "Enemy.hpp"
 #include <cmath>
-
-class Enemy : public Entity {
-public:
-    Enemy(sf::Vector2f pos);
-    void update(float dt) override;
-    void draw(sf::RenderWindow& window) const override;
-    sf::FloatRect getBounds() const override;
-    
-private:
-    sf::CircleShape shape;
-    float speed = 150.0f;
-    float swayAmplitude = 50.0f;
-    float swayFrequency = 2.0f;
-    float time = 0;
-    float startX;
-};
 
 Enemy::Enemy(sf::Vector2f pos) {
     shape.setRadius(20);
